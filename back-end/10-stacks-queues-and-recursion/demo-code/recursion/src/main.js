158c231bc0e2b6c90f8f9291aa7ab579751ebaaa
vladimirsan <vladimirsan@gmail.com>
2018-09-27 14:24:10 -0700

    day 14

--- back-end/10-stacks-queues-and-recursion/demo-code/recursion/src/main.js
+++ back-end/10-stacks-queues-and-recursion/demo-code/recursion/src/main.js
@@ -57,13 +57,13 @@
 // bigONSpace([1, 2, 4, 5, 6]);
 
 
-// const simpleLoop = () => {
+// const simpleLoop = () => { // O(1) time and O(1)
 //   for (let i = 0; i < 10; i++) {
 //     console.log(i);
 //   }
 // };
 //
-// const simpleRecursion = (counter) => {
+// const simpleRecursion = (counter) => { // O(1) space O(1)
 //   // --------------------------------------------
 //   // BASE CASE
 //   // --------------------------------------------
@@ -76,39 +76,37 @@
 //   console.log(counter);
 //   simpleRecursion(counter += 1);
 // };
-//
-// simpleRecursion(0);
-// simpleLoop();
+
+//simpleRecursion(0);
+//simpleLoop();
 
 
-// const addition = (a, b) => {
+// const addition = (a, b) => { // O(1) time and space
 //   return a + b;
 // };
 
 
-// const recursiveAddition = (a, b) => {
-//   if (b === 0) {
-//     return a;
-//   }
-//   return recursiveAddition(a + 1, b - 1);
-// };
-//
-// console.log(recursiveAddition(5, 7));
-
-
-
-const sampleArray = [1 ,2 ,3];
-let sum = 0;
-
-for (let i = 0; i < sampleArray.length; i++) {
-  sum += sampleArray[i];
-}
-
-console.log(sum);
-
-const recursiveArrayAddition = (array) => {
-  return recursiveArrayAdditionHelper(array, 0);
+const recursiveAddition = (a, b) => { // O(B) Space O(B) Time
+  if (b === 0) {
+    return a;
+  }
+  return recursiveAddition(a + 1, b - 1);
 };
+
+console.log(recursiveAddition(0, 100));
+
+// const sampleArray = [1 ,2 ,3];
+// let sum = 0;
+//
+// for (let i = 0; i < sampleArray.length; i++) {
+//   sum += sampleArray[i];
+// }
+//
+// console.log(sum);
+//
+// const recursiveArrayAddition = (array) => {
+//   return recursiveArrayAdditionHelper(array, 0);
+// };
 
 // const recursiveArrayAdditionHelper = (array, currentIndex, sumSoFar) => {
 //   //! Vinicio - Base case
@@ -120,37 +118,51 @@ const recursiveArrayAddition = (array) => {
 //   //! Vinicio - Recursive Case
 // };
 
-const recursiveArrayAdditionHelper = (array, currentIndex) => {
-  if (currentIndex === array.length - 1) {
-    //! Vinicio - this is the LAST call of the function
-    return array[currentIndex];
+// const recursiveArrayAdditionHelper = (array, currentIndex) => {
+//   if (currentIndex === array.length - 1) {
+//     //! Vinicio - this is the LAST call of the function
+//     return array[currentIndex];
+//   }
+//
+//   //! Vinicio - recursive case
+//   const restOfTheProblem = recursiveArrayAdditionHelper(array, currentIndex + 1);
+//   return array[currentIndex] + restOfTheProblem;
+// };
+//
+// console.log(recursiveArrayAddition([1, 2, 3]));
+
+
+// class Stack{
+//   constructor() {
+//     //! Vinicio - we are not supposed to use internalStorage directly
+//     this.internalStorage = [];
+//   }
+//
+//   push(value) {
+//     this.internalStorage.unshift(value);
+//   }
+//
+//
+//   pop(value) {
+//     return this.internalStorage.shift(value);
+//   }
+//
+//   //! Vinicio - using ONLY push and pop guarantees LIFO
+// }
+
+
+
+class AnimalShelter(){
+  constructor(){
+    this.cats = new Queue();
+    this.dogs = new Queue();
+    this.serial = 0;
   }
 
-  //! Vinicio - recursive case
-  const restOfTheProblem = recursiveArrayAdditionHelper(array, currentIndex + 1);
-  return array[currentIndex] + restOfTheProblem;
-};
-
-console.log(recursiveArrayAddition([1, 2, 3]));
-
-
-class Stack{
-  constructor() {
-    //! Vinicio - we are not supposed to use internalStorage directly
-    this.internalStorage = [];
+  enqueue(animal){
+    animal.serial = serial; // new Date();
+    serial++;
+    // insert into queues
   }
-
-  push(value) {
-    this.internalStorage.unshift(value);
-  }
-
-  pop(value) {
-    this.internalStorage.shift(value);
-  }
-  //! Vinicio - using ONLY push and pop guarantees LIFO
 }
-
-
-
-
 
