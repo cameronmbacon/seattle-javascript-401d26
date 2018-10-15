ddc5676189e8aa7a6d45bf50550d7fcae6913324
vladimirsan <vladimirsan@gmail.com>
2018-10-15 12:14:58 -0700

    day 26

--- back-end/19-bst/demo-code/trees/src/main.js
+++ back-end/19-bst/demo-code/trees/src/main.js
@@ -1,30 +1,45 @@
 'use strict';
 
-const BST = require('./lib/binary-search-tree');
+const array = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
 
-class Node {
-  constructor(value) {
-    this.left = null;
-    this.right = null;
-    this.value = value;
+// Let's use row and column
+for (let row = 0; row < array.length; row++) {
+  for (let col = 0; col < array[row].length; col++) {
+    console.log(array[row][col]);
   }
 }
 
-
-const myBST = new BST();
-
-myBST.insert(new Node(10));
-myBST.insert(new Node(5));
-myBST.insert(new Node(15));
-myBST.insert(new Node(3));
-myBST.insert(new Node(11));
-
-for (let i = 0; i < 20; i++) {
-  myBST.insert(new Node(100 + i));
+for (let col = 0; col < array[0].length; col++) {
+  for (let row = 0; row < array.length; row++) {
+    console.log(array[row][col]);
+  }
 }
 
-
-myBST.find(6);
-console.log(myBST.steps);
-myBST.find(119);
-console.log(myBST.steps);
+// const BST = require('./lib/binary-search-tree');
+//
+// class Node {
+//   constructor(value) {
+//     this.left = null;
+//     this.right = null;
+//     this.value = value;
+//   }
+// }
+//
+//
+// const myBST = new BST();
+//
+// myBST.insert(new Node(10));
+// myBST.insert(new Node(5));
+// myBST.insert(new Node(15));
+// myBST.insert(new Node(3));
+// myBST.insert(new Node(11));
+//
+// for (let i = 0; i < 20; i++) {
+//   myBST.insert(new Node(100 + i));
+// }
+//
+//
+// myBST.find(6);
+// console.log(myBST.steps);
+// myBST.find(119);
+// console.log(myBST.steps);
